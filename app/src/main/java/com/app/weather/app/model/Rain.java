65905e70d5bf69c07485182012f87f1f0be5e8f2
szymonaszek01 <235879@edu.p.lodz.pl>
2023-04-15 15:34:50 +0200

    task-003 Adding models

---
+++ app/src/main/java/com/app/weather/app/model/Rain.java
@@ -0,0 +1,18 @@
+package com.app.weather.app.model;
+
+public class Rain {
+
+    private double _1h;
+
+    public Rain(double _1h) {
+        this._1h = _1h;
+    }
+
+    public double get_1h() {
+        return _1h;
+    }
+
+    public void set_1h(double _1h) {
+        this._1h = _1h;
+    }
+}
