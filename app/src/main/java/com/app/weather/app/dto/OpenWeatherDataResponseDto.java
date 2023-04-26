b01e289284e3a2f4f35107249ee74464efb61a37
szymonaszek01 <235879@edu.p.lodz.pl>
2023-04-27 00:07:54 +0200

    task-014 Editing dto class

--- app/src/main/java/com/app/weather/app/dto/OpenWeatherDataResponseDto.java
+++ app/src/main/java/com/app/weather/app/dto/OpenWeatherDataResponseDto.java
@@ -1,184 +1,79 @@
 package com.app.weather.app.dto;
 
-import com.app.weather.app.model.Clouds;
-import com.app.weather.app.model.Coord;
-import com.app.weather.app.model.Main;
-import com.app.weather.app.model.Rain;
-import com.app.weather.app.model.Sys;
-import com.app.weather.app.model.Weather;
-import com.app.weather.app.model.Wind;
+import com.app.weather.app.model.Current;
+import com.app.weather.app.model.Daily;
 import com.google.gson.annotations.SerializedName;
 
 public class OpenWeatherDataResponseDto {
 
-    @SerializedName("coord")
-    private Coord coord;
+    private double lat;
 
-    @SerializedName("weather")
-    private Weather[] weather;
+    private double lon;
 
-    @SerializedName("base")
-    private String base;
+    private String timezone;
 
-    @SerializedName("main")
-    private Main main;
+    @SerializedName("timezone_offset")
+    private int timezoneOffset;
 
-    @SerializedName("visibility")
-    private int visibility;
+    private Current current;
 
-    @SerializedName("wind")
-    private Wind wind;
+    @SerializedName("daily")
+    private Daily[] dailyList;
 
-    @SerializedName("rain")
-    private Rain rain;
-
-    @SerializedName("clouds")
-    private Clouds clouds;
-
-    @SerializedName("dt")
-    private long dt;
-
-    @SerializedName("sys")
-    private Sys sys;
-
-    @SerializedName("timezone")
-    private int timezone;
-
-    @SerializedName("id")
-    private long id;
-
-    @SerializedName("name")
-    private String name;
-
-    @SerializedName("cod")
-    private int cod;
-
-    public OpenWeatherDataResponseDto(Coord coord, Weather[] weather, String base, Main main, int visibility, Wind wind, Rain rain, Clouds clouds, long dt, Sys sys, int timezone, long id, String name, int cod) {
-        this.coord = coord;
-        this.weather = weather;
-        this.base = base;
-        this.main = main;
-        this.visibility = visibility;
-        this.wind = wind;
-        this.rain = rain;
-        this.clouds = clouds;
-        this.dt = dt;
-        this.sys = sys;
+    public OpenWeatherDataResponseDto(double lat, double lon, String timezone, int timezoneOffset, Current current, Daily[] dailyList) {
+        this.lat = lat;
+        this.lon = lon;
         this.timezone = timezone;
-        this.id = id;
-        this.name = name;
-        this.cod = cod;
+        this.timezoneOffset = timezoneOffset;
+        this.current = current;
+        this.dailyList = dailyList;
     }
 
-    public Coord getCoord() {
-        return coord;
+    public double getLat() {
+        return lat;
     }
 
-    public void setCoord(Coord coord) {
-        this.coord = coord;
+    public void setLat(double lat) {
+        this.lat = lat;
     }
 
-    public Weather[] getWeather() {
-        return weather;
+    public double getLon() {
+        return lon;
     }
 
-    public void setWeather(Weather[] weather) {
-        this.weather = weather;
+    public void setLon(double lon) {
+        this.lon = lon;
     }
 
-    public String getBase() {
-        return base;
-    }
-
-    public void setBase(String base) {
-        this.base = base;
-    }
-
-    public Main getMain() {
-        return main;
-    }
-
-    public void setMain(Main main) {
-        this.main = main;
-    }
-
-    public int getVisibility() {
-        return visibility;
-    }
-
-    public void setVisibility(int visibility) {
-        this.visibility = visibility;
-    }
-
-    public Wind getWind() {
-        return wind;
-    }
-
-    public void setWind(Wind wind) {
-        this.wind = wind;
-    }
-
-    public Rain getRain() {
-        return rain;
-    }
-
-    public void setRain(Rain rain) {
-        this.rain = rain;
-    }
-
-    public Clouds getClouds() {
-        return clouds;
-    }
-
-    public void setClouds(Clouds clouds) {
-        this.clouds = clouds;
-    }
-
-    public long getDt() {
-        return dt;
-    }
-
-    public void setDt(long dt) {
-        this.dt = dt;
-    }
-
-    public Sys getSys() {
-        return sys;
-    }
-
-    public void setSys(Sys sys) {
-        this.sys = sys;
-    }
-
-    public int getTimezone() {
+    public String getTimezone() {
         return timezone;
     }
 
-    public void setTimezone(int timezone) {
+    public void setTimezone(String timezone) {
         this.timezone = timezone;
     }
 
-    public long getId() {
-        return id;
+    public int getTimezoneOffset() {
+        return timezoneOffset;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setTimezoneOffset(int timezoneOffset) {
+        this.timezoneOffset = timezoneOffset;
     }
 
-    public String getName() {
-        return name;
+    public Current getCurrent() {
+        return current;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCurrent(Current current) {
+        this.current = current;
     }
 
-    public int getCod() {
-        return cod;
+    public Daily[] getDailyList() {
+        return dailyList;
     }
 
-    public void setCod(int cod) {
-        this.cod = cod;
+    public void setDailyList(Daily[] dailyList) {
+        this.dailyList = dailyList;
     }
 }
